18b8ca7122dd81e4063a489dc727d3d52a1669f9
Kojo Kumah <info@kojokumah.com>
2020-10-18 16:56:29 -0700

    Initial check in of animation test project

---
+++ Source/ThirdPersonScratch/ThirdPersonScratch.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "ThirdPersonScratch.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThirdPersonScratch, "ThirdPersonScratch" );
+ 
